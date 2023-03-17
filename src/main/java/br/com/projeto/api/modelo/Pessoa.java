d1dbd7d8e3233aa895233ab00c7b6bb4ee8179ef
Veridiana <veridiana17timoteo@gmail.com>
2023-03-17 17:52:36 -0300

    Aprendendo a utilizar algumas annotations para gerar tabelas

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,9 +1,27 @@
 package br.com.projeto.api.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
     private String nome;
     private int idade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 
     public String getNome() {
         return nome;
